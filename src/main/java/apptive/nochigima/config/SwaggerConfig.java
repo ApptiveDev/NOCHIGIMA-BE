699a12c8125a84476a8257cbec9560f8fbba6b5b
doorcs <itsdoorcs@gmail.com>
2025-11-10 20:51:54 +0900

    feat: Swagger API 문서 추가

---
+++ src/main/java/apptive/nochigima/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package apptive.nochigima.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().info(info()).servers(List.of(server()));
+    }
+
+    private Info info() {
+        return new Info().title("놓치지마");
+    }
+
+    private Server server() {
+        return new Server().url("/").description("놓치지마 API v1");
+    }
+}
