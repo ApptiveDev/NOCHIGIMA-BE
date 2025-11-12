6c858d1dcdf62f86db07f43cacd2f7f30f6aa25b
doorcs <itsdoorcs@gmail.com>
2025-11-12 13:45:21 +0900

    chore: Spring Retry 설정 및 의존성 추가

---
+++ src/main/java/apptive/nochigima/config/RetryConfig.java
@@ -0,0 +1,8 @@
+package apptive.nochigima.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+@Configuration
+@EnableRetry
+public class RetryConfig {}
